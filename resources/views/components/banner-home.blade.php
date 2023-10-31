ae6999bd4937cf83ba61189c2a9f1d68e7441290
mthiagoalves <m.thiago.alves@live.com>
2023-10-31 12:28:10 +0000

    change how call asset image

--- resources/views/components/banner-home.blade.php
+++ resources/views/components/banner-home.blade.php
@@ -1,8 +1,8 @@
 <div class="container-fluid p-0">
     <div class="col-12 p-0 position-relative">
         <div class="blur"></div>
-        <img src="/img/homepage/banner-initial.png" alt="banner" class="img-fluid d-none d-sm-block">
-        <img src="/img/homepage/banner-initial-m.png" alt="banner" class="img-fluid d-block d-sm-none">
+        <img src="{{asset('/img/homepage/banner-initial.png')}}" alt="banner" class="img-fluid d-none d-sm-block">
+        <img src="{{asset('/img/homepage/banner-initial-m.png')}}" alt="banner" class="img-fluid d-block d-sm-none">
         <div class="located-in-pt d-none d-sm-block">
             <div class="img-located-in-pt">
                 <img src="{{ asset('/img/world.png') }}" alt="world" class="img-fluid rotate-image">
